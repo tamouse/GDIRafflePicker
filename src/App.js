4326c7aa1bcdd391960163615dccbaf0bb7bae60
Tamara Temple <tamouse@gmail.com>
2016-12-04 02:11:56 -0600

    Rearrange components, models, and assets
    * move React Components under components/
    * move / create models for stores under models/
    * move logo and data files under assets/
    * add png logo
    
    WIre up some actions
    
    * onKeyDown in NameLIst input box
    * onClick in WinnerList panel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import './App.css'
-import Header from './Header'
-import Footer from './Footer'
-import NameList from './NameList'
-import WinnerList from './WinnerList'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+import NameList from './components/NameList'
+import WinnerList from './components/WinnerList'
 
 class App extends Component {
   render() {
