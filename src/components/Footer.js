4036fba88b801f3078083b1e0df82288d7f29cdd
Tamara Temple <tamouse@gmail.com>
2016-12-12 23:24:43 -0600

    fix footer link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
     return (
       <footer className="Footer">
         <span className="Footer__copyright">Written with &hearts; by&nbsp;
-          <a href="{tamouse.url}">{tamouse.nickName}</a>.</span>
+          <a href={tamouse.url}>{tamouse.nickName}</a>.</span>
         <span className="Footer__acknowledgements">&hearts; React. <ReactLogo/></span>
       </footer>
     )
